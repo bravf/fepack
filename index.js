6769f2176adf8fd20a5381d0534d62789fc2c1b7
bravf <bravfing@126.com>
2016-11-08 11:30:37 +0800

    update: 发布1.1.9版本，主要解决js文件被tsc编译问题

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ let util = require('./src/util')
 
 function main(){
     program
-        .version('1.1.8')
+        .version('1.1.9')
         .option('init', 'create fepack.json', _=>{
             createConfig()
         })
