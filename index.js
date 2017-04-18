63bb7d22d0932940ada4514961f26c620b6e1590
bravf <bravfing@126.com>
2017-04-18 10:41:51 +0800

    add: add jade markdown inline tool

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ let util = require('./src/util')
 
 function main(){
     program
-        .version('2.2.4')
+        .version('2.2.5')
         .option('init', 'create fepack.json', _=>{
             createConfig()
         })
