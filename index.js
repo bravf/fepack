bd67bf1fc675a49a10bc787e2bd239644fe51836
bravf <bravfing@126.com>
2017-12-12 16:56:08 +0800

    change: 发布 2.4.5, 修复 page 下 下划线开头 jade 文件忽略问题

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ let util = require('./src/util')
 
 function main(){
     program
-        .version('2.4.4')
+        .version('2.4.5')
         .option('init', 'create fepack.json', _=>{
             createConfig()
         })
